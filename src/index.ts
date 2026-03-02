badccb2250827643f0d1898605912829e490d5e4
SohamShah20 <sohamjshah20@gmail.com>
2026-03-02 18:57:48 +0530

    Prisma Setup
    Added Prisma setup with Contact schema and migration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+import { prisma } from "./config/prisma";
 
 dotenv.config();
+
+prisma.$connect()
+  .then(() => console.log("Database connected"))
+  .catch(err => console.error("DB connection error:", err));
 
 const app = express();
 
